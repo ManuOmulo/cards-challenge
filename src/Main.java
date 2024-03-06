970a3ef025cf41bf91ba5c265b878be68ed40ee3
ManuOmulo <omuloemmanuel@gmail.com>
2024-03-06 18:46:45 +0300

    Added the Card class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import deck.Card;
+
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-
+    List<Card> deck = Card.getStandardDeck();
+    Card.printDeck(deck);
   }
 }
